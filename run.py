bfd953ff13630279d4c093be394e2e9b5b2ffbfb
mgltorsa <mgltorsa@gmail.com>
2019-04-03 22:53:40 -0500

    added choices

--- run.py
+++ run.py
@@ -5,6 +5,10 @@ from experto.sistema import *
 engine = Experto()
 engine.reset()
 
-engine.declare(Campo(ph=7.2))
+
+engine.declare(Campo(PH=choice([Ph.ALCALINO,Ph.LIGERAMENTE_ALCALINO,Ph.ACIDO])))
+engine.declare(Campo(CE=choice([Ce.ALTA,Ce.BAJA])))
+engine.declare(Campo(arcilla=choice([40.0,89.0,75])))
+
 
 engine.run()
